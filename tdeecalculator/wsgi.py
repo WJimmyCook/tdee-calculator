8c267a68b4952e4a8aba8755b9a91a4009f34c79
Jimmy Cook <wjimmycook@gmail.com>
2017-10-31 08:00:07 -0400

    tdee calculator project created

---
+++ tdeecalculator/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tdeecalculator project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tdeecalculator.settings")
+
+application = get_wsgi_application()
