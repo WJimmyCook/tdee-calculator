38b5d12e6726f1c7d342f83f9e41bcf77c8e610f
Jimmy Cook <wjimmycook@gmail.com>
2017-11-11 11:31:34 -0500

    Internationalization and localization

--- tdeecalculator/settings/base.py
+++ tdeecalculator/settings/base.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
+LANGUAGES = (
+    ('en', _('English')),
+    ('ca', _('Catalan')),
+)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -43,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'tdeecalculator.urls'
@@ -56,6 +62,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -108,3 +115,7 @@ def get_env_variable(var_name):
         raise ImproperlyConfigured(error_msg)
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
--- tdeecalculator/urls.py
+++ tdeecalculator/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from .views import home, home_files
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', home, name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name='home-files'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += i18n_patterns(
+    url(r'^$', home, name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+)
